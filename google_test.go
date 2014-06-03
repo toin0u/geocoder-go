d37c43c79f1f175dd049dfee2632865953f0418d
Antoine Corcy <dev@sbin.dk>
2014-06-03 20:14:55 +0200

    add google provider test

---
+++ google_test.go
@@ -0,0 +1,33 @@
+package geocoder
+
+import (
+	"testing"
+)
+
+func TestGeocode(t *testing.T) {
+	var google Google
+	address := Address{"Paris, France"}
+
+	coordinate, err := google.Geocode(address)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if coordinate.Lat != 48.856614 || coordinate.Lng != 2.3522219 {
+		t.Errorf("Wrong coordinate: %v", coordinate)
+	}
+}
+
+func TestReverse(t *testing.T) {
+	var google Google
+	coodinate := Coordinate{48.856614, 2.3522219}
+
+	address, err := google.Reverse(coodinate)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if address.Street != "94 Quai de l'Hôtel de ville, 75004 Paris, France" {
+		t.Errorf("Wrong address: %v", address)
+	}
+}
